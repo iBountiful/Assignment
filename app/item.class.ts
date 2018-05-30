98913c358a8ad85e2acb0c5643e48585ebe4cbb9
iBountiful <39746475+iBountiful@users.noreply.github.com>
2018-05-30 15:22:59 -0700

    Add files via upload

---
+++ app/item.class.ts
@@ -0,0 +1,10 @@
+export class item {
+//variables with types specified
+name : string;
+quantity : number;
+//default constructor
+  constructor(p1: string, p2: number){
+    this.name = p1;
+    this.quantity = p2;
+  }
+}
---
+++ app/item.spec.ts
@@ -0,0 +1,11 @@
+import {item} from "./item.class";
+
+describe("Placeholder Suite", function () {
+  let name = "carrot";
+  let amt =5;
+  let item1 = new item(name, amt);
+  beforeEach(function () {
+
+  });
+
+});
